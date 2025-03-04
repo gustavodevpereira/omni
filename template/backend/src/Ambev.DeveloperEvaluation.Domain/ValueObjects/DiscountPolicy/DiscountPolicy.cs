125c188d588d646fd3849be7fd7efbcc48d9a330
gustavodevpereira <gustavdrp@icloud.com>
2025-03-04 15:56:14 -0300

    #1 - refactor discount policy into an layered strategy structure

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/ValueObjects/DiscountPolicy/DiscountPolicy.cs
@@ -0,0 +1,29 @@
+﻿using Ambev.DeveloperEvaluation.Domain.ValueObjects.DiscountPolicy.Strategies;
+
+namespace Ambev.DeveloperEvaluation.Domain.ValueObjects.DiscountPolicy;
+
+public record DiscountPolicy
+{
+    private readonly List<(Func<int, bool> Condition, IDiscountStrategy Strategy)> _strategies;
+
+    public DiscountPolicy()
+    {
+        _strategies =
+        [
+            (q => q < 4, new NoDiscountStrategy()),
+            (q => q >= 4 && q < 10, new TenPercentDiscountStrategy()),
+            (q => q >= 10 && q <= 20, new TwentyPercentDiscountStrategy())
+        ];
+    }
+
+    public decimal GetDiscountPercentage(int quantity)
+    {
+        var selectedStrategy = _strategies.FirstOrDefault(s => s.Condition(quantity));
+
+        if (selectedStrategy.Equals(default))
+            throw new DomainException("The quantity cannot be higher than 20");
+
+        return selectedStrategy.Strategy.CalculateDiscount(quantity);
+    }
+
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/ValueObjects/DiscountPolicy/IDiscountStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.Domain.ValueObjects.DiscountPolicy
+{
+    public interface IDiscountStrategy
+    {
+        decimal CalculateDiscount(int quantity);
+    }
+}
