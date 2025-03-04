caf3a5851e30818d5f0f6379f9cff7ef0c39fd73
gustavodevpereira <gustavdrp@icloud.com>
2025-03-04 15:14:55 -0300

    #1 - add discount policy value object and its unit tests

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/ValueObjects/DiscountPolicy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.Domain.ValueObjects
+{
+    public record DiscountPolicy
+    {
+        public decimal GetDiscountPercentage(int quantity)
+        {
+            if (quantity < 4)
+                return 0m;
+            if (quantity >= 4 && quantity < 10)
+                return 0.1m;  // 10% de desconto
+            if (quantity >= 10 && quantity <= 20)
+                return 0.2m;  // 20% de desconto
+
+            throw new ArgumentException("A quantidade não pode exceder 20.");
+        }
+    }
+}
