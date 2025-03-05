c4260a2f9e4fdae73f3e6d881b316fc8a98b9ba7
gustavodevpereira <gustavdrp@icloud.com>
2025-03-05 07:20:08 -0300

    #5 - add create sale handlers as it unit testing

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleCommand.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.CreateSale
+{
+    /// <summary>
+    /// Command for creating a new sale.
+    /// </summary>
+    public class CreateSaleCommand : IRequest<CreateSaleResult>
+    {
+        public string SaleNumber { get; set; } = string.Empty;
+        public DateTime SaleDate { get; set; }
+        public string CustomerExternalId { get; set; } = string.Empty;
+        public string CustomerName { get; set; } = string.Empty;
+        public string BranchExternalId { get; set; } = string.Empty;
+        public string BranchName { get; set; } = string.Empty;
+
+        // Lista de itens da venda
+        public List<CreateSaleItemCommand> Items { get; set; } = new List<CreateSaleItemCommand>();
+    }
+
+    /// <summary>
+    /// Command for creating a sale item.
+    /// </summary>
+    public class CreateSaleItemCommand
+    {
+        public string ProductExternalId { get; set; } = string.Empty;
+        public string ProductName { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Ambev.DeveloperEvaluation.Application.Sales.CreateSale
+{
+    /// <summary>
+    /// Represents the result after creating a sale.
+    /// </summary>
+    public class CreateSaleResult
+    {
+        public Guid Id { get; set; }
+    }
+}
