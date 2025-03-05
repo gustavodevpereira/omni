02bf50203c22bc0230988c8f712f4f80cc4f0ba5
gustavodevpereira <gustavdrp@icloud.com>
2025-03-05 05:48:16 -0300

    #3 - add sales repository

---
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Repositories/SaleRepository.cs
@@ -0,0 +1,27 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities.Sales;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ambev.DeveloperEvaluation.ORM.Repositories
+{
+    public class SaleRepository : ISaleRepository
+    {
+        private readonly DefaultContext _context;
+
+        public SaleRepository(DefaultContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Sale?> GetSaleByIdAsync(int saleId, CancellationToken cancellationToken)
+        {
+            return await _context.Sales.FindAsync([saleId], cancellationToken);
+        }
+
+        public async Task UpdateSaleAsync(Sale sale, CancellationToken cancellationToken)
+        {
+            _context.Sales.Update(sale);
+            await Task.CompletedTask;
+        }
+    }
+}
