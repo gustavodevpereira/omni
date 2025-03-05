7b3b98104277b8b7e22e3431f24562fbb9661e84
gustavodevpereira <gustavdrp@icloud.com>
2025-03-05 05:46:20 -0300

    #3 - add domain events and handlers

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/SaleModifiedHandler.cs
@@ -0,0 +1,12 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Events.Sales;
+using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales;
+
+public class SaleModifiedHandler : INotificationHandler<SaleModifiedEvent>
+{
+    public Task Handle(SaleModifiedEvent notification, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
